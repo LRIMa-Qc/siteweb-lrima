f9bedbdc899eb323377071862ddb94e4c8ade300
noahfavreau <noahjamesfavreau@gmail.com>
2026-03-01 12:12:10 -0500

    Add mobile menu, made the audience section look better on mobile

---
+++ src/components/ui/icons/BurgerIcon.tsx
@@ -0,0 +1,21 @@
+import { ComponentProps } from 'react'
+
+export function BurgerIcon({ className, ...props }: ComponentProps<'svg'>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      {...props}
+    >
+      <path d="M4 6h16M4 12h16M4 18h16" />
+    </svg>
+  )
+}
